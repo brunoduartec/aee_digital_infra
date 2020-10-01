3455202198a9d6962968c9396f70517e88377ec2
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-10-01 19:04:13 -0300

    Update to add trabalhos api

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 
 echo "Inicializando"
 
-systemctl stop mysqld
-
-docker login https://docker.pkg.github.com -u brunoduartec -p 5c0d229c8d57e3e96aab4df2e294d0b00a27c4a2
+docker login https://docker.pkg.github.com -u brunoduartec -p df643f7375d20f14dca570d592381011a1a0440b
 
 echo "Baixando a imagem da API"
 
@@ -12,5 +10,7 @@ docker pull docker.pkg.github.com/brunoduartec/aee_tech/aeeapi:latest
 echo "Baixando a imagem do front"
 
 docker pull docker.pkg.github.com/brunoduartec/aee_tech_front/aeetechfront:latest
+
+systemctl stop mysqld
 
 docker-compose up --build -d
