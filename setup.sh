2101b6fa7099b6577874cfe2f5a76a222da0729d
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-10-02 16:33:51 -0300

    removes disable mysql

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 
 echo "Inicializando"
 
-systemctl stop mysqld
-
 docker login https://docker.pkg.github.com -u brunoduartec -p 5c0d229c8d57e3e96aab4df2e294d0b00a27c4a2
 
 echo "Baixando a imagem da API"
