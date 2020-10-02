7c3c9f4ad13aa7bfe8d7a98479197f07d664a68e
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2020-10-02 17:03:30 -0300

    update to use safe login

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 echo "Inicializando"
 
-docker login https://docker.pkg.github.com -u brunoduartec -p 5c0d229c8d57e3e96aab4df2e294d0b00a27c4a2
+cat TOKEN.txt | docker login https://docker.pkg.github.com -u brunoduartec --password-stdin
 
 echo "Baixando a imagem da API"
 
