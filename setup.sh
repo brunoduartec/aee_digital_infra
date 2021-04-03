a38bca3f5f7d05c68d8fae4c7288e8228d35af33
Bruno Duarte <bruno.duartec@gmail.com>
2021-04-03 10:30:32 -0300

    update to new project name images

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ cat ~/DOCKER_TOKEN.txt | docker login https://docker.pkg.github.com -u brunoduar
 
 echo "Baixando a imagem das APIs"
 
-docker pull docker.pkg.github.com/brunoduartec/aee_tech/aeeapi:latest
+docker pull docker.pkg.github.com/brunoduartec/aee_digital_regionais/aee_digital_regionais:latest
 
 docker pull docker.pkg.github.com/brunoduartec/aee_digital_trabalhos/aee_digital_trabalhos:latest
 
 echo "Baixando a imagem do front"
 
-docker pull docker.pkg.github.com/brunoduartec/aee_tech_front/aeetechfront:latest
+docker pull docker.pkg.github.com/brunoduartec/aee_digital_front/aee_digital_front:latest
 
 docker-compose up --build -d
