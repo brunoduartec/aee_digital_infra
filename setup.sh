9fd0720c5005bea60d36c2ccd85015b3505b5a56
Bruno Duarte <bruno.duartec@gmail.com>
2021-04-29 11:04:00 -0300

    simplify setup

--- setup.sh
+++ setup.sh
@@ -5,21 +5,10 @@ echo "Inicializando"
 
 cat ~/DOCKER_TOKEN.txt | docker login https://docker.pkg.github.com -u brunoduartec --password-stdin
 
-echo "Baixando a imagem das APIs"
+echo "Baixando imagens"
 
-docker pull docker.pkg.github.com/brunoduartec/aee_digital_regionais/aee_digital_regionais:latest
-
-docker pull docker.pkg.github.com/brunoduartec/aee_digital_trabalhos/aee_digital_trabalhos:latest
-
-
-echo "Baixando a imagem do front"
-
-docker pull docker.pkg.github.com/brunoduartec/aee_digital_front/aee_digital_front:latest
-
-echo "Baixando a imagem do Infra"
-
-docker pull docker.pkg.github.com/brunoduartec/aee_digital_infra/aee_digital_infra:latest
+docker-compose pull
 
 echo "Deploying....."
 
-docker-compose up --build -d
+docker-compose up -d
